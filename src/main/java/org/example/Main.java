119744cca07b2b7b7abb858f33d3a137d37f4b93
Yojahida Mónica <45293103@continental.edu.pe>
2024-11-07 16:06:29 -0500

    Creación de un objeto ejemplo.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,24 @@
 package org.example;
 
+import org.example.negocio.AccountType;
 import org.example.persistencia.BankService;
 
 public class Main {
     public static void main(String[] args) {
         BankService bankService = new BankService();
-
+        //Register a new client
         bankService.registerClient("Mónica", "Arroyo", "45293103", "yojahidaa@gmail.com");
+        // open a bank account
+        bankService.openBankAccount("45293103", AccountType.SAVINGS);
+        // Deposit money
+        bankService.deposit("45293103", "1000001", 1500.0);
+        // Check balance
+        System.out.println("Balance: " + bankService.checkBalance("45293103", "1000001"));
 
-
+        // Withdraw money
+        bankService.withdraw("45293103", "1000001", 900.0);
+        // Check balance again
+        System.out.println("Balance: " + bankService.checkBalance("45293103", "1000001"));
 
     }
 }
